c93818e9339cfd2a27d89df4bf06b5ad1b12e87e
yasmimx <101806624+yasmimx@users.noreply.github.com>
2022-03-25 10:46:05 -0300

    add código

--- src/exercico1/Exercico1.java
+++ src/exercico1/Exercico1.java
@@ -1,37 +1,85 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exercico1;
+package Exercicio1;
 
-import java.lang.reflect.Array;
-import static java.lang.reflect.Array.get;
 import java.util.Scanner;
-import static jdk.nashorn.internal.objects.NativeString.charAt;
 
 /**
  *
  * @author 2etimds
  */
-public class Exercico1 {
+public class Exercicio1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Digite um número entre 0 e 999");
-        String num = entrada.next();
-
-        
-        char centena = num.charAt(0);
-        char dezena = num.charAt(1);
-        char unidade = num.charAt(2);
-
-
+        yasmim();
     }
-       
+    public static void extenso(String a, int b){
+        String[][] unidade = {
+            {"","", "", ""},
+            
+            {"onze","um", "dez", "cento"}, 
+            
+            {"doze", "dois", "vinte", "duzentos"},
+            
+            {"treze", "três", "trinta", "trezentos"}, 
+            
+            {"quatorze", "quatro", "quarenta", "quatrocentos"},
+            
+            {"quinze", "cinco", "cinquenta", "quinhentos"}, 
+            
+            {"dezesseis", "seis", "sessenta", "seiscentos"}, 
+            
+            {"dezesete", "sete", "setenta", "setecentos"}, 
+            
+            {"dezoito", "oito", "oitenta", "oitocentos"}, 
+            
+            {"dezenove", "nove", "noventa", "novecentos"} 
+        };
+        System.out.print(unidade[ Integer.parseInt((a)) ][b]); 
     }
+    public static void yasmim(){
+        String num;
+        try (Scanner leitor = new Scanner(System.in)) {
+            System.out.println("Digite um número");
+            num = leitor.nextLine();
+        }
+        
+        String cen = "",
+                dez = "",
+                uni = "";
+
+        uni += num.charAt(num.length() - 1);
+
+        if(num.length() > 2){ 
+            cen += num.charAt( num.length() - 3 ); 
+            if(num.charAt( num.length() - 3 ) != '0'){ 
+                extenso(cen, 3);
+                System.out.print(" e ");
+
+            }
+        }
+        
+        if(num.length() > 1){ 
+            dez += num.charAt( num.length() - 2); 
+            if(num.charAt( num.length() - 2) != '0') { 
+                if(num.charAt( num.length() - 2) == '1') 
+                    extenso(uni, 0);
+                else{
+    
+                    extenso(dez, 2);
+                    System.out.print(" e ");
+                }
+            }
+        }
+        
+        try {
+            if(num.charAt( num.length() - 2) != '1')
+            extenso(uni, 1);
+        } catch (Exception e) {
+            extenso(uni, 1);
+        }
+        
+    }
+}
 
